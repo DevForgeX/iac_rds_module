3affa1ce828a2e03717b5b6e5aa6eb607b636b0e
Christopher Junior Afeku <afekuchris@gmail.com>
2024-04-11 14:28:36 +0000

    update: rewrote rds module

--- rds.tf
+++ rds.tf
@@ -10,51 +10,38 @@ resource "aws_db_subnet_group" "main" {
   )
 }
 
-resource "aws_rds_cluster" "main" {
-    # for_each = data.aws_availability_zones.main
-  cluster_identifier      = var.database_cluster_identidier
-  engine                  = var.database_cluster_engine
-  engine_version = var.database_cluster_engine_version
-  availability_zones      =   [for az in data.aws_availability_zones.main.names : az]              # ["us-west-2a", "us-west-2b", "us-west-2c"]
-  database_name           = var.database_db_name
-  master_username         = var.database_cluster_db_username
-  master_password         = var.database_cluster_db_password
-  apply_immediately       = var.database_cluster_apply_changes_immediately
-  db_subnet_group_name = aws_db_subnet_group.main.name
-  deletion_protection = var.database_cluster_delete_protection
-  enabled_cloudwatch_logs_exports = var.database_cluster_cloudwatch_log_exports
-  backup_retention_period = var.database_cluster_backup_retention_period
-  preferred_backup_window = var.database_cluster_preferrred_backup_window
-  storage_encrypted = var.database_cluster_storage_encrypted
-  vpc_security_group_ids = var.database_cluster_vpc_security_group_ids
-  final_snapshot_identifier = var.database_cluster_final_snapshot_identifier
-  skip_final_snapshot = var.database_cluster_skip_final_snapshot
-  allocated_storage = var.database_cluster_allocated_storage
-  storage_type = var.database_cluster_storage_type
-  db_cluster_instance_class = var.database_cluster_instance_class
+resource "aws_db_instance" "main" {
+  count = var.database_instance_instance_count
+  identifier              = "${var.database_instance_identifier_prefix}-${count.index}"
+  engine                  = var.database_instance_engine
+  engine_version          = var.database_instance_engine_version
+  instance_class          = var.database_instance_class
+  allocated_storage       = var.database_instance_allocated_storage
+  max_allocated_storage   = var.database_instance_allocated_storage
+  db_name                 = var.database_db_name
+  username                = var.database_instance_db_username
+  password                = var.database_instance_db_password
+  parameter_group_name    = var.db_instance_parameter_group_name
+  backup_retention_period = var.database_instance_backup_retention_period
+  vpc_security_group_ids  = var.database_instance_vpc_security_group_ids
+  db_subnet_group_name    = aws_db_subnet_group.main.name
+  skip_final_snapshot     = var.database_instance_skip_final_snapshot
+  multi_az = var.database_instance_enable_multi_az
+  deletion_protection = var.database_instance_delete_protection
+  storage_encrypted = var.database_instance_storage_encrypted
+  final_snapshot_identifier = var.database_instance_final_snapshot_identifier
+  storage_type = var.database_instance_storage_type
 
-
+  timeouts {
+    create = "1h"
+    delete = "1h"
+    update = "1h"
+  }
 
     tags = merge(
     var.tags,
     {
-      Name = "${var.global_var_org_tag}-${var.global_var_product_tag}-${var.global_var_environment_tag}-cluster"
-    }
-  )
-}
-
-resource "aws_rds_cluster_instance" "cluster_instances" {
-  count              = var.database_cluster_instance_count
-  identifier         = "${var.database_cluster_instance_identifier_prefix}-${count.index}"
-  cluster_identifier = aws_rds_cluster.main.id
-  instance_class     = var.database_cluster_instance_class
-  engine             = aws_rds_cluster.main.engine
-  engine_version     = aws_rds_cluster.main.engine_version
-
-  tags = merge(
-    var.tags,
-    {
-      Name = "${var.global_var_org_tag}-${var.global_var_product_tag}-${var.global_var_environment_tag}-subnet-group"
+      Name = "${var.global_var_org_tag}-${var.global_var_product_tag}-${var.global_var_environment_tag}-db"
     }
   )
 }
--- variable.tf
+++ variable.tf
@@ -4,19 +4,13 @@ variable "database_subnet_group_name" {
   description = "Default Database Subnet Group Name"
 }
 
-variable "database_cluster_identidier" {
+variable "database_instance_engine" {
   type = string
-  default = "odoo-postgres-cluster"
-  description = "Default DB Cluster Identifier"
-}
-
-variable "database_cluster_engine" {
-  type = string
-  default = "aurora-postgresql"
+  default = "postgresql"
   description = "Default Cluster Engine"
 }
 
-variable "database_cluster_engine_version" {
+variable "database_instance_engine_version" {
   type = string
   default = "16"
 }
@@ -27,22 +21,16 @@ variable "database_db_name" {
   description = "Dafault Database Name"
 }
 
-variable "database_cluster_db_username" {
+variable "database_instance_db_username" {
   type = string
   default = "foo"
   description = "Default DB Username"
 }
 
-variable "database_cluster_db_password" {
+variable "database_instance_db_password" {
   type = string
   default = "foobarbar12345"
   description = "Default DB Password"
-}
-
-variable "database_cluster_apply_changes_immediately" {
-  type = bool
-  default = true
-  description = "Trigger To Allow Immediate Application Of Changes"
 }
 
 variable "tags" {
@@ -75,81 +63,73 @@ variable "database_subnet_group_subnet_ids" {
   default = [ "subnet-0bff39194476e14be", "subnet-0179e0979ca6b0163", "	subnet-02c3bbb6cf1005d26" ]
 }
 
-variable "database_cluster_delete_protection" {
+variable "database_instance_delete_protection" {
   type = bool
   default = false
   description = "Delete Protection Trigger For Cluster"
 }
 
-variable "database_cluster_cloudwatch_log_exports" {
-  type = list(string)
-  default = ["postgresql"]
-  description = "Default Logs Type To Send To Cloudwatch"
+variable "database_instance_enable_multi_az" {
+  type = bool
+  default = false
 }
 
-# variable "database_cluster_engine_version" {
-#   type = string
-#   default = "value"
-#   description = "Default DB Cluster Engine Version"
-# }
-
-variable "database_cluster_backup_retention_period" {
+variable "database_instance_backup_retention_period" {
   type = number
   default = 5
   description = "Default Data Retention Period"
 }
 
-variable "database_cluster_preferrred_backup_window" {
+variable "db_instance_parameter_group_name" {
   type = string
-  default = "11:00-00:00"
-  description = "Default Data Retention Period"
+  default = "odoo-db-parameter-group"
 }
 
 
-variable "database_cluster_vpc_security_group_ids" {
+variable "database_instance_vpc_security_group_ids" {
   type = list(string)
   default = [ "sg-0b560917e4287612b" ]
   description = "List Of Security Group For DB Cluster"
 }
 
-variable "database_cluster_storage_encrypted" {
+variable "database_instance_storage_encrypted" {
   type = bool
   default = false
   description = "Enable encryption for DB storage"
 }
 
-variable "database_cluster_instance_count" {
+variable "database_instance_instance_count" {
   type = number
   default = 1
   description = "Default value for number of DB Instances"
 }
 
-variable "database_cluster_instance_identifier_prefix" {
+variable "database_instance_identifier_prefix" {
   type = string
   default = "odoo-cluster-instance"
 }
 
-variable "database_cluster_instance_class" {
+variable "database_instance_class" {
   type = string
   default = "db.t3.medium"
 }
 
-variable "database_cluster_final_snapshot_identifier" {
+variable "database_instance_final_snapshot_identifier" {
   type = string
   default = "odoo-final-rds-snapshot"
 }
 
-variable "database_cluster_skip_final_snapshot" {
+variable "database_instance_skip_final_snapshot" {
   type = bool
   default = true
 }
 
-variable "database_cluster_allocated_storage" {
+variable "database_instance_allocated_storage" {
   type = number
   default = 10
 }
 
-variable "database_cluster_storage_type" {
+variable "database_instance_storage_type" {
   type = string
   default = "gp3"
 }
